2f6b5a36cbf09db9411f2f666df5eb55097742d8
Rithvik2008 <73972258+Rithvik2008@users.noreply.github.com>
2020-12-01 18:58:33 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Desining-algortihm
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,32 @@
+var movingRect, fixedRect
+
+function setup() {
+  createCanvas(1200,800);
+  movingRect=createSprite(400, 800, 80, 50);
+  movingRect.shapeColor="red"
+  fixedRect=createSprite(400,100,50,80)
+  fixedRect.shapeColor="red"
+  movingRect.velocityY=-5
+  fixedRect.velocityY=+5
+}
+
+function draw() {
+  background(0);  
+
+
+
+if(movingRect.x-fixedRect.x < (movingRect.width + fixedRect.width)/2 &&
+fixedRect.x-movingRect.x < (movingRect.width + fixedRect.width)/2){
+  movingRect.velocityX=movingRect.velocityX*(-1)
+  fixedRect.velocityX=fixedRect.velocityX*(-1)
+}
+ if (movingRect.y-fixedRect.y < (movingRect.height + fixedRect.height)/2 &&
+fixedRect.y-movingRect.y < (movingRect.height + fixedRect.height)/2){
+  movingRect.velocityY=movingRect.velocityY*(-1)
+  fixedRect.velocityY=fixedRect.velocityY*(-1)
+}
+
+
+
+  drawSprites();
+}
